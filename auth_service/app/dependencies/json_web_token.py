372fcd490b71bea46065e9206e0e8d5fc3a7a38d
Pablo Salinas <salinas.pablom@gmail.com>
2022-12-06 00:16:02 -0300

    Add JWT related functions

---
+++ auth_service/app/dependencies/json_web_token.py
@@ -0,0 +1,51 @@
+from datetime import (
+    timezone,
+    datetime,
+    timedelta
+)
+
+import jwt
+
+from settings import get_settings
+
+
+def create_jwt(user_id) -> str:
+    '''
+    Creates a JWT (JSON web token), encrypting the username
+    as part of the payload.
+    Returns the str representing the JWT.
+
+    :param user_id: int representing the user the token belongs to.
+    :return str: JWT for authentication purposes.
+    '''
+    settings = get_settings()
+    delta = settings.token_expiration
+    expiration_date = datetime.now(timezone.utc) + timedelta(minutes=delta)
+    payload = {
+        'user_id': str(user_id),
+        'exp': expiration_date,
+    }
+    return jwt.encode(payload, settings.jwt_secret)
+
+
+def extract_jwt_payload(json_web_token: str) -> dict:
+    '''
+    Extracts the payload from the provided JWT.
+    Returns a dict object containing the authentication
+    info of the user the JWT belongs to.
+
+    :param json_web_token: str web token.
+    :return dict object.
+    '''
+    try:
+        return jwt.decode(
+            json_web_token,
+            settings.jwt_secret,
+            options={'require': ['exp']}
+        )
+    except jwt.ExpiredSignatureError:
+        raise ExpiredToken()
+
+
+class ExpiredToken(Exception):
+    pass
