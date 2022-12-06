8d8035db4220c8ec8ff794f9f2ad95bbcb791c3f
Pablo Salinas <salinas.pablom@gmail.com>
2022-12-06 00:16:18 -0300

    Add JWT-related settings

--- auth_service/app/settings.py
+++ auth_service/app/settings.py
@@ -4,13 +4,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    # private_key_file: str = '/usr/local/bin/private.pem'
     db_user: str
     db_pass: str
     db_host: str
     db_port: str
     password_min_length: int
     salt_rounds: int
+    token_expiration: int
+    jwt_secret: str = 'secret'
 
 
 @lru_cache
