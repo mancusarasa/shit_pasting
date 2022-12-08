0282bb09d1f7949c6e88169a59091f66deeceabf
Pablo Salinas <salinas.pablom@gmail.com>
2022-12-08 16:14:39 -0300

    Use Depends() to validate authentication/extract user_id from Headers

--- paste_service/app/routers/pastes.py
+++ paste_service/app/routers/pastes.py
@@ -1,21 +1,29 @@
-from fastapi import APIRouter, Header
+from fastapi import (
+    APIRouter,
+    Header,
+    Depends
+)
+
+from dependencies.authentication import extract_current_user
 
 
 router = APIRouter()
 
 
-# @router.get(
-#     '',
-#     tags=['pastes'],
-#     status_code=200
-# )
-# def retrieve_pastes(token: ):
-#     pass
+@router.get(
+    '/myPastes',
+    tags=['myPastes'],
+    status_code=200
+)
+def retrieve_pastes(user_id: str = Depends(extract_current_user)):
+    return {
+        'pastes': []
+    }
 
 
-# @router.post(
-#     '',
-#     tags=['pastes'],
-# )
-# def create_paste():
-#     pass
+@router.post(
+    '/myPastes',
+    tags=['myPastes'],
+)
+def create_paste():
+    pass
