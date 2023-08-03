c6f45da89128e2126ffb2a8276984e0e07ede179
Pablo Salinas <salinas.pablom@gmail.com>
2023-08-03 13:01:13 -0300

    Remove useless django project

--- web/web_ui/pastes_ui/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def index(request):
-    return HttpResponse('Hello world!')
