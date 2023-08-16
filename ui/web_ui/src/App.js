3c15eff895d254e1bf85fe0df54a4394fb9336f1
Pablo Salinas <salinas.pablom@gmail.com>
2023-08-16 11:31:04 -0300

    Retab everything

--- ui/web_ui/src/App.js
+++ ui/web_ui/src/App.js
@@ -6,33 +6,33 @@ import LoginPage from './LoginPage.js'
 
 
 export default function App() {
-    const [authToken, dispatch] = useReducer(
-        authReducer,
-        null
-    );
+  const [authToken, dispatch] = useReducer(
+    authReducer,
+    null
+  );
 
-    return (
-        <>
-            <AuthContext.Provider value={authToken}>
-                <AuthDispatchContext.Provider value={dispatch}>
-                    <LoginPage/>
-                    {(authToken !== null) && <p> logged in! </p>}
-                </AuthDispatchContext.Provider>
-            </AuthContext.Provider>
-        </>
-    );
+  return (
+    <>
+      <AuthContext.Provider value={authToken}>
+        <AuthDispatchContext.Provider value={dispatch}>
+          <LoginPage/>
+          {(authToken !== null) && <p> logged in! </p>}
+        </AuthDispatchContext.Provider>
+      </AuthContext.Provider>
+    </>
+  );
 }
 
 function authReducer(authToken, action) {
-    switch (action.type) {
-        case 'logged_in': {
-            return action.authToken;
-        }
-        case 'logged_out': {
-            return null;
-        }
-        default: {
-            throw new Error('unknown action: ' + action.type);
-        }
+  switch (action.type) {
+    case 'logged_in': {
+      return action.authToken;
     }
+    case 'logged_out': {
+      return null;
+    }
+    default: {
+      throw new Error('unknown action: ' + action.type);
+    }
+  }
 }
--- ui/web_ui/src/LoginPage.js
+++ ui/web_ui/src/LoginPage.js
@@ -7,89 +7,87 @@ import './LoginPage.css';
 
 function LoginForm() {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    const dispatch = useContext(AuthDispatchContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const dispatch = useContext(AuthDispatchContext);
 
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const host = process.env.REACT_APP_AUTH_SERVICE_HOST;
-        const port = process.env.REACT_APP_AUTH_SERVICE_PORT;
-        const authServiceUrl = 'http://' + host + ':' + port + '/login'
-        await fetch(authServiceUrl, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        }).then(response => {
-            return response.json();
-        }).then (jsonValue => {
-            if (jsonValue.error) {
-                setError(jsonValue.error);
-            } else {
-                setError(null);
-                dispatch({
-                    type: 'logged_in',
-                    authToken: jsonValue.auth_token
-                });
-            }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const host = process.env.REACT_APP_AUTH_SERVICE_HOST;
+    const port = process.env.REACT_APP_AUTH_SERVICE_PORT;
+    const authServiceUrl = 'http://' + host + ':' + port + '/login'
+    await fetch(authServiceUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+      username: username,
+      password: password
+    })
+    }).then(response => {
+      return response.json();
+    }).then (jsonValue => {
+      if (jsonValue.error) {
+      setError(jsonValue.error);
+      } else {
+      setError(null);
+      dispatch({
+        type: 'logged_in',
+        authToken: jsonValue.auth_token
+      });
+    }
+    })
+    .catch(error => {
+      console.log('caught something?');
+      setError(error);
+    });
+  };
 
-        })
-        .catch(error => {
-            console.log('caught something?');
-            setError(error);
-        });
-    };
-
-    return (
-        <div className="login-form">
-            <div className="title"> Sign In </div>
-            <div className="form">
-                <form onSubmit={handleSubmit}>
-                    <div className="username-container">
-                        <label>Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            required
-                            onChange={
-                                (event) => setUsername(event.target.value)
-                            }
-                        />
-                    </div>
-                    <div className="password-container">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            required
-                            onChange={
-                                (event) => setPassword(event.target.value)
-                            }
-                        />
-                        {(error !== null) && <div className="error">{error}</div>}
-                    </div>
-                    <div className="button-container">
-                        <input type ="submit" value="Submit"/>
-                    </div>
-                    
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-form">
+      <div className="title"> Sign In </div>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="username-container">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              required
+              onChange={
+                (event) => setUsername(event.target.value)
+              }
+            />
+          </div>
+          <div className="password-container">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              required
+              onChange={
+                (event) => setPassword(event.target.value)
+              }
+            />
+            {(error !== null) && <div className="error">{error}</div>}
+          </div>
+          <div className="button-container">
+            <input type ="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 
 export default function LoginPage() {
-    return (
-        <>
-            <div className="login-page">
-                <LoginForm/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="login-page">
+        <LoginForm/>
+      </div>
+    </>
+  );
 }
