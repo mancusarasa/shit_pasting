55dcd85c404d97ae54c790a262cc74e95fa09664
Pablo Salinas <salinas.pablom@gmail.com>
2023-08-18 19:09:52 -0300

    Use the new AppRouter component

--- ui/web_ui/src/App.js
+++ ui/web_ui/src/App.js
@@ -2,21 +2,18 @@ import { useReducer } from 'react';
 
 import { AuthContext, AuthDispatchContext } from './AuthContext.js';
 
-import LoginPage from './LoginPage.js'
+import LoginPage from './LoginPage.js';
+import AppRouter from './AppRouter.js'
 
 
 export default function App() {
-  const [authToken, dispatch] = useReducer(
-    authReducer,
-    null
-  );
+  const [authToken, dispatch] = useReducer(authReducer, null);
 
   return (
     <>
-      <AuthContext.Provider value={authToken}>
+    <AuthContext.Provider value={authToken}>
         <AuthDispatchContext.Provider value={dispatch}>
-          <LoginPage/>
-          {(authToken !== null) && <p> logged in! </p>}
+          {authToken !== null ? <AppRouter/ > : <LoginPage/>}
         </AuthDispatchContext.Provider>
       </AuthContext.Provider>
     </>
