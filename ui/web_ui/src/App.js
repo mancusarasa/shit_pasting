32f78979d88c8c112231f47113a59b5d70a02514
Pablo Salinas <salinas.pablom@gmail.com>
2023-08-18 19:11:40 -0300

    Fix tabs

--- ui/web_ui/src/App.js
+++ ui/web_ui/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <>
-    <AuthContext.Provider value={authToken}>
+      <AuthContext.Provider value={authToken}>
         <AuthDispatchContext.Provider value={dispatch}>
           {authToken !== null ? <AppRouter/ > : <LoginPage/>}
         </AuthDispatchContext.Provider>
