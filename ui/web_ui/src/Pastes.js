2c8b0be8da4ee86d488cdc6dff6155b4666057f0
Pablo Salinas <salinas.pablom@gmail.com>
2023-08-18 20:15:07 -0300

    Add some interaction with the paste_service
    It doesn't work yet, it's returning 403

--- ui/web_ui/src/Pastes.js
+++ ui/web_ui/src/Pastes.js
@@ -1,4 +1,33 @@
+import { useEffect, useContext } from 'react';
+
+import { AuthContext } from './AuthContext.js';
+
+
 export default function Pastes() {
+
+  const authToken = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchPastes = async () => {
+      const host = process.env.REACT_APP_PASTE_SERVICE_HOST;
+      const port = process.env.REACT_APP_PASTE_SERVICE_PORT;
+      const pasteServiceUrl = 'http://' + host + ':' + port + '/myPastes';
+      await fetch(pasteServiceUrl, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json', 'Authentication': 'Bearer '  + authToken}
+      }).then(response => {
+        return response.json();
+      }).then(jsonValue => {
+        console.log('these are my pastes:');
+        console.log(jsonValue);
+      }).catch(error => {
+        console.log(error);
+      });
+
+    };
+    fetchPastes();
+  });
+
   return (
     <div>My Pastes!</div>
   );
