0319ab4cdbb4872634ab1c9a5f7a19faeeec93ca
Pablo Salinas <salinas.pablom@gmail.com>
2024-05-05 17:12:56 -0300

    Store auth_token in a cookie instead of a state variable

--- ui/web_ui/src/App.js
+++ ui/web_ui/src/App.js
@@ -1,19 +1,21 @@
 import { useReducer } from 'react';
+import { Cookies } from 'react-cookie';
 
 import { AuthContext, AuthDispatchContext } from './AuthContext.js';
 
 import LoginPage from './LoginPage.js';
 import AppRouter from './AppRouter.js'
 
+const cookies = new Cookies();
 
 export default function App() {
-  const [authToken, dispatch] = useReducer(authReducer, null);
+  const [authToken, dispatch] = useReducer(authReducer, cookies.get('auth_token'));
 
   return (
     <>
       <AuthContext.Provider value={authToken}>
         <AuthDispatchContext.Provider value={dispatch}>
-          {authToken !== null ? <AppRouter/ > : <LoginPage/>}
+          {authToken !== undefined ? <AppRouter/ > : <LoginPage/>}
         </AuthDispatchContext.Provider>
       </AuthContext.Provider>
     </>
@@ -23,10 +25,12 @@ export default function App() {
 function authReducer(authToken, action) {
   switch (action.type) {
     case 'logged_in': {
+      cookies.set('auth_token', action.authToken);
       return action.authToken;
     }
     case 'logged_out': {
-      return null;
+      cookies.remove('auth_token')
+      return undefined;
     }
     default: {
       throw new Error('unknown action: ' + action.type);
