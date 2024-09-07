80829a6a4efbd85be9af0d95596ddcd067953711
Pablo Salinas <salinas.pablom@gmail.com>
2024-09-07 13:14:14 -0300

    Include the sidebar in the home content component

--- new_ui/web_ui/components/home-content.tsx
+++ new_ui/web_ui/components/home-content.tsx
@@ -2,6 +2,7 @@
 
 import { Navbar } from "@/components/navbar";
 import { AuthContext, authReducer, initialState, AuthDispatchContextType } from "@/components/auth-context";
+import { Sidebar } from "@/components/sidebar";
 import { useReducer } from "react";
 
 export const HomeContent = ({children,}: {children: React.ReactNode;}) => {
@@ -14,10 +15,11 @@ export const HomeContent = ({children,}: {children: React.ReactNode;}) => {
   return (
     <>
       <Navbar />
+      <Sidebar />
       <AuthContext.Provider value={value}>
-          <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-            {children}
-          </main>
+        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+          {children}
+        </main>
       </AuthContext.Provider>
     </>
   );
