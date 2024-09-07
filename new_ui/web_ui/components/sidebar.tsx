2a2e15ae09c313b3395daee42c6fb5232accf976
Pablo Salinas <salinas.pablom@gmail.com>
2024-09-07 20:03:38 -0300

    Remove unused imports

--- new_ui/web_ui/components/sidebar.tsx
+++ new_ui/web_ui/components/sidebar.tsx
@@ -1,16 +1,13 @@
 "use client";
-import * as React from "react";
-import { IconSvgProps } from "@/types";
-import { Link } from "@nextui-org/link";
-import { siteConfig } from "@/config/site";
 import NextLink from "next/link";
 import {
   HomeIcon,
   SettingsIcon,
   PowerIcon,
-  UsersIcon,
+  ProfileIcon,
   NewPasteIcon,
-  MyPastesIcon
+  MyPastesIcon,
+  MyFeedIcon
 } from "@/components/icons";
 
 export const Sidebar = () => {
@@ -25,33 +22,21 @@ export const Sidebar = () => {
             </NextLink>
           </li>
           <li>
-            <NextLink href={'/settings'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
-              <SettingsIcon className="w-5 h-5 mr-3" />
-              <span>Settings</span>
-            </NextLink>
-          </li>
-          <li>
-            <NextLink href={'/support'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
-              <PowerIcon className="w-5 h-5 mr-3" />
-              <span>Support</span>
-            </NextLink>
-          </li>
-          <li>
-            <NextLink href={'/profile'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
-              <UsersIcon className="w-5 h-5 mr-3" />
-              <span>Profile</span>
-            </NextLink>
-          </li>
-          <li>
             <NextLink href={'/compose'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
               <NewPasteIcon className="w-5 h-5 mr-3" />
               <span>New Paste</span>
             </NextLink>
           </li>
           <li>
-            <NextLink href={'/my_pastes'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
+            <NextLink href={'/feed'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
               <MyPastesIcon className="w-5 h-5 mr-3" />
               <span>My Pastes</span>
+            </NextLink>
+          </li>
+          <li>
+            <NextLink href={'/my_pastes'} className="flex items-center px-6 py-3 text-white hover:bg-gray-700">
+              <MyFeedIcon className="w-5 h-5 mr-3" />
+              <span>Feed</span>
             </NextLink>
           </li>
         </ul>
