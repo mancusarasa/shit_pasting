90ce4b7f3b4dced746ceee5aded75ef8c23f79a7
Pablo Salinas <salinas.pablom@gmail.com>
2024-09-11 19:59:16 -0300

    Readd middleware to ask for authentication in some places

--- new_ui/web_ui/middleware.tsx
+++ new_ui/web_ui/middleware.tsx
@@ -1,47 +1,49 @@
 import { NextResponse, NextRequest } from 'next/server';
+import { Cookies } from "react-cookie";
 // import { cookies } from "@/components/auth-context";
 
-// async function getAuthStatus(): Promise<boolean> {
 
-//   const token = cookies.get('auth_token');
-//   const host = process.env.NEXT_PUBLIC_AUTH_SERVICE_HOST;
-//   const port = process.env.NEXT_PUBLIC_AUTH_SERVICE_PORT;
-//   const authServiceUrl = `http://${host}:${port}/userinfo`;
+async function getAuthStatus(): Promise<boolean> {
 
-//   fetch(authServiceUrl, {
-//     method: 'GET',
-//     headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization': `Bearer ${token}`
-//     }
-//   }).then(response => { 
-//     console.log(response);
-//     if (response.status != 200) {
-//       return false;
-//     } else {
-//       console.log(response.json());
-//       return true;
-//     }
-//   }).catch(error => {
-//     return false;
-//   });
-//   return false;
-// }
+  const cookies = new Cookies();
+  const token = cookies.get('auth_token');
+  const host = process.env.NEXT_PUBLIC_AUTH_SERVICE_HOST;
+  const port = process.env.NEXT_PUBLIC_AUTH_SERVICE_PORT;
+  const authServiceUrl = `http://${host}:${port}/userinfo`;
 
-export async function middleware(request: NextRequest) {
-  return NextResponse.next();
-  // const isAuthenticated: boolean = await getAuthStatus();
-  // if (isAuthenticated) {
-  //     return NextResponse.next();
-  // } else {
-  //     return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  fetch(authServiceUrl, {
+    method: 'GET',
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+    }
+  }).then(response => { 
+    console.log(response);
+    if (response.status != 200) {
+      return false;
+    } else {
+      console.log(response.json());
+      return true;
+    }
+  }).catch(error => {
+    return false;
+  });
+  return false;
 }
 
-// export const config = {
-//   matcher: [
-//     '/',
-//     '/home',
-//     '/!(login)',
-//   ],
-// }
+export async function middleware(request: NextRequest) {
+  const isAuthenticated: boolean = await getAuthStatus();
+  if (isAuthenticated) {
+      return NextResponse.next();
+  } else {
+      return NextResponse.redirect(new URL('/login', request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/my_pastes",
+    "/compose",
+    "/feed",
+  ],
+}
