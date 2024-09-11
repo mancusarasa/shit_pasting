a538097788e01df6b806491df9f180c3e2fffa01
Pablo Salinas <salinas.pablom@gmail.com>
2024-09-11 01:34:29 -0300

    Add empty pages for compose/, feed/, logout/, my_pastes/, profile/ and settings/

---
+++ new_ui/web_ui/app/feed/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function FeedPage() {
+	return (
+		<div>
+			<h1 className={title()}>Feed</h1>
+		</div>
+	);
+}
---
+++ new_ui/web_ui/app/logout/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function LogoutPage() {
+	return (
+		<div>
+			<h1 className={title()}>Logout</h1>
+		</div>
+	);
+}
