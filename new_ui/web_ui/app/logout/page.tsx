41de79e3a74cc9360fa6e7ce2b11d9cb608c656b
Pablo Salinas <salinas.pablom@gmail.com>
2024-10-26 20:03:04 -0300

    Implement logic to log out current user

--- new_ui/web_ui/app/logout/page.tsx
+++ new_ui/web_ui/app/logout/page.tsx
@@ -1,9 +1,21 @@
-import { title } from "@/components/primitives";
+"use client"
+
+import { useContext } from 'react';
+import { useEffect } from 'react'
+import { useRouter } from "next/navigation";
+import { AuthContext } from "@/components/auth-context";
 
 export default function LogoutPage() {
-	return (
-		<div>
-			<h1 className={title()}>Logout</h1>
-		</div>
-	);
+  const { state, dispatch } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    dispatch({
+      event_type: 'logged_out'
+    });
+    router.push("/");
+    router.refresh();
+  }, [router, dispatch]);
+
+  return <div></div>;
 }
