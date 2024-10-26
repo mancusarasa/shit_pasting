1dc1c26c04fe29d5c56a57f5ae8ddb5385ddc61b
Pablo Salinas <salinas.pablom@gmail.com>
2024-10-26 20:01:44 -0300

    Simplify middleware logic

--- new_ui/web_ui/middleware.tsx
+++ new_ui/web_ui/middleware.tsx
@@ -1,44 +1,16 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { Cookies } from "react-cookie";
-// import { cookies } from "@/components/auth-context";
 
-
-async function getAuthStatus(): Promise<boolean> {
-
-  const cookies = new Cookies();
-  const token = cookies.get('auth_token');
-  const host = process.env.NEXT_PUBLIC_AUTH_SERVICE_HOST;
-  const port = process.env.NEXT_PUBLIC_AUTH_SERVICE_PORT;
-  const authServiceUrl = `http://${host}:${port}/userinfo`;
-
-  fetch(authServiceUrl, {
-    method: 'GET',
-    headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-    }
-  }).then(response => { 
-    console.log(response);
-    if (response.status != 200) {
-      return false;
-    } else {
-      console.log(response.json());
-      return true;
-    }
-  }).catch(error => {
-    return false;
-  });
-  return false;
-}
-
-export async function middleware(request: NextRequest) {
-  const isAuthenticated: boolean = await getAuthStatus();
-  if (isAuthenticated) {
-      return NextResponse.next();
+export function middleware(request: NextRequest) {
+  const authToken = request.cookies.get("auth_token");
+  if (!authToken) {
+    request.cookies.delete("auth_token");
+    const loginUrl = new URL("/login", request.url)
+    // loginUrl.searchParams.set('from', request.nextUrl.pathname);
+    const response = NextResponse.redirect(loginUrl);
+    response.cookies.delete("auth_token");
+    return response;
   } else {
-      const loginUrl = new URL('/login', request.url)
-      // loginUrl.searchParams.set('from', request.nextUrl.pathname);
-      return NextResponse.redirect(loginUrl);
+    return NextResponse.next();
   }
 }
 
