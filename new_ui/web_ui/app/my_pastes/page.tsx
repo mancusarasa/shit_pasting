43921c960bb32f0cf45c0be84a655548b5875494
Pablo Salinas <salinas.pablom@gmail.com>
2024-10-27 15:30:22 -0300

    Retrieve paste_service/my_pastes when loading /my_pastes

--- new_ui/web_ui/app/my_pastes/page.tsx
+++ new_ui/web_ui/app/my_pastes/page.tsx
@@ -1,6 +1,48 @@
+"use client";
+
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from "next/navigation";
+import { AuthContext } from "@/components/auth-context";
 import { title } from "@/components/primitives";
 
 export default function MyPastesPage() {
+
+  const router = useRouter();
+  const {state, dispatch} = useContext(AuthContext);
+  const [pastes, setPastes] = useState([]);
+
+  useEffect(() => {
+    const fetchPastes = async () => {
+      const host = process.env.NEXT_PUBLIC_PASTE_SERVICE_HOST;
+      const port = process.env.NEXT_PUBLIC_PASTE_SERVICE_PORT;
+      const pasteServiceUrl = `http://${host}:${port}/my_pastes`;
+      return await fetch(pasteServiceUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${state.auth_token}`
+        }
+      }).then(response => {
+        return response.json().then(data => ({
+          status: response.status,
+          data: data
+        }));
+      }).then(result => {
+        if (result.status === 403) {
+          dispatch({event_type: "logged_out"});
+          router.push("/");
+          router.refresh();
+        } else if (result.status === 200) {
+            console.log(result.data.pastes);
+            setPastes(result.data.pastes);
+        }
+      }).catch(error => {
+        throw error;
+      });
+    };
+    fetchPastes();
+  }, [router, state, dispatch]);
+
 	return (
 		<div>
 			<h1 className={title()}>MyPastes</h1>
