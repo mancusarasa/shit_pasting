a8a273fb5ce972a376675d8b333589015ae6a10d
Pablo Salinas <salinas.pablom@gmail.com>
2024-11-11 20:25:11 -0300

    Add an initial shitty version of the register page

--- new_ui/web_ui/app/register/page.tsx
+++ new_ui/web_ui/app/register/page.tsx
@@ -1,9 +1,43 @@
 import { title } from "@/components/primitives";
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import { Card, CardBody } from "@nextui-org/card";
 
 export default function SettingsPage() {
 	return (
 		<div>
-			<h1 className={title()}>Register</h1>
+      <Card
+        className="p-4 max-w-md"
+      >
+        <CardBody>
+          <div>Create your account</div>
+          <div>
+            <Input
+              type="text"
+              label="Username"
+              size="sm"
+              variant="faded"
+            />
+            <Input
+              type="password"
+              label="Password"
+              size="sm"
+              variant="faded"
+            />
+            <Input
+              type="password"
+              label="Confirm your password"
+              size="sm"
+              variant="faded"
+            />
+            <Button
+              size="sm"
+            >
+              Go!
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
 		</div>
 	);
 }
